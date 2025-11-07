f71255a65de4336df7ca7a64f977a86c0b15e108
Debadipta Sahu <debssahu123@gmail.com>
2025-11-07 23:19:36 +0530

    added kth smallest element

---
+++ DSA_91.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class DSA_91 {
+    
+
+    public static int kthLargestelement(int[] arr, int k){
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> (b-a));
+        for(int num: arr){
+            queue.add(num);
+            if(queue.size() > k){
+                queue.poll();
+            }
+        }
+        return queue.peek();
+
+
+    }
+    public static void main(String[] args){
+        int[] arr = {21, 31, 14, 15, 16, 18, 19};
+        int k = 3;
+        int result = kthLargestelement(arr, k);
+        System.out.println("THE "+ k +" SMALLEST ELEMeNT : "+result);
+
+    }
+}
+
+
